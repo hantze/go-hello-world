ec111117a39307870abd6250d0a895d17822ba74
hantze <hantze.sudarma@payfazz.com>
2018-10-17 16:54:10 +0700

    change

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,45 @@ package main
 
 import "fmt"
 
+type Anu struct {
+	Property int
+}
+
+func (a *Anu) test1() {
+	fmt.Println("anu")
+}
+
+type Anu2 struct {
+	Anu
+}
+
+func (a *Anu2) test1() {
+	fmt.Println("anu2")
+}
+
+func myTest2(a Anu) {
+	fmt.Println("test 2 ", a.Property)
+}
+
+func myTest() *Anu2 {
+	return &Anu2{
+		Anu{
+			Property: 12,
+		},
+	}
+}
+
 func main() {
-   fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	a := Anu2{}
+	a.Anu.test1()       // call from Parent
+	a.Anu.Property = 10 // set property
+	fmt.Println(a.Anu.Property)
+	a.test1() // overriding
+
+	myTest2(a.Anu)
+
+	temp := myTest()               // call myTest
+	fmt.Println(temp.Property)     // direct access
+	fmt.Println(temp.Anu.Property) // access via parent
 }
